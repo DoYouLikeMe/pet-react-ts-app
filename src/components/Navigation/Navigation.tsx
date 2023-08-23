352a408a2f82402325d99c6128429fe6d8c2a705
ALEXEY <avoronsib@gmail.com>
2023-08-23 13:25:26 +0300

    add: navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,30 @@
+import {NavLink} from "react-router-dom";
+
 export const Navigation = () => {
   return (
-    <nav>
-      <ul></ul>
+    <nav className="navigation navigation_main">
+      <ul className="navigation__list">
+        <li className="navigation__item">
+          <NavLink className="navigation__link" to="/">
+            About the shelter
+          </NavLink>
+        </li>
+        <li className="navigation__item">
+          <NavLink className="navigation__link" to="/our-pets">
+            Our Pets
+          </NavLink>
+        </li>
+        <li className="navigation__item">
+          <NavLink className="navigation__link" to="/help">
+            Help the shelter
+          </NavLink>
+        </li>
+        <li className="navigation__item">
+          <NavLink className="navigation__link" to="/contacts">
+            Contacts
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
